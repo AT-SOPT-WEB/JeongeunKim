518518a621cc0c35dd3e9fcf5774031ad0556089
Jeong-Ag <kjungu1105@gmail.com>
2025-04-18 01:06:55 +0900

    feat: 전체, 완료, 미완료 버튼 기능 추가

---
+++ assignment2/js/todo.js
@@ -0,0 +1,56 @@
+import { getTodo, setTodo } from "./utils.js";
+import { renderTableHead, renderTableBody } from "./table.js";
+
+class Todo {
+  constructor({
+    todoHeader,
+    todoBody,
+    entireBtn,
+    completeBtn,
+    incompleteBtn,
+    priorityBtn,
+    priorityDropdown,
+    searchInput,
+    prioritySelect,
+    addBtn,
+    deleteBtn,
+    completeToggleBtn,
+    priority,
+    headList,
+  }) {
+    this.todoList = getTodo();
+    this.todoBody = todoBody;
+    this.todoHeader = todoHeader;
+    this.searchInput = searchInput;
+    this.prioritySelect = prioritySelect;
+    this.priorityDropdown = priorityDropdown;
+
+    entireBtn.addEventListener("click", () => this.showEntireList());
+    completeBtn.addEventListener("click", () => this.showCompleteList());
+    incompleteBtn.addEventListener("click", () => this.showIncompleteList());
+  }
+
+  // to do 리스트 나타내기 (기본값은 local storage에 저장된 todoList)
+  render(list = this.todoList) {
+    renderTableBody(list, this.todoBody);
+  }
+
+  // 전체 리스트 조회
+  showEntireList() {
+    this.render();
+  }
+
+  // 완료 리스트 조회
+  showCompleteList() {
+    const filtered = this.todoList.filter((todo) => todo.completed);
+    this.render(filtered);
+  }
+
+  // 미완료 리스트 조회
+  showIncompleteList() {
+    const filtered = this.todoList.filter((todo) => !todo.completed);
+    this.render(filtered);
+  }
+}
+
+export default Todo;
