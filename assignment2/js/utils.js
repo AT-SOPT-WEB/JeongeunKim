a9afeacfaee3da56aaad1c34ca3764ea93029858
Jeong-Ag <kjungu1105@gmail.com>
2025-04-18 01:14:10 +0900

    feat: 전체 체크박스 기능 추가

--- assignment2/js/utils.js
+++ assignment2/js/utils.js
@@ -8,3 +8,20 @@ export function getTodo() {
   localStorage.setItem("todoList", JSON.stringify(todos));
   return [...todos];
 }
+
+export function handleSelectAll(e) {
+  const isChecked = e.target.checked;
+  const checkboxes = document.querySelectorAll(".todo-checkbox");
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = isChecked;
+  });
+}
+
+export function updateSelectAllCheckbox() {
+  const selectAllCheckbox = document.getElementById("select-all-checkbox");
+  const checkboxes = document.querySelectorAll(".todo-checkbox");
+  const allChecked = Array.from(checkboxes).every(
+    (checkbox) => checkbox.checked
+  );
+  selectAllCheckbox.checked = allChecked;
+}
