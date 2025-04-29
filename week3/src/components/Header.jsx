2814ad6eb914e86a6ebb2eba570bb9aa04a21c90
Jeong-Ag <kjungu1105@gmail.com>
2025-04-29 17:15:31 +0900

    feat: 3주차 실습

---
+++ week3/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import style from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <nav className={style.header}>
+      <h1>스웹팟 파트원</h1>
+    </nav>
+  );
+};
+
+export default Header;
