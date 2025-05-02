2a1153554ab69253cf82cba9d160494e7d610a02
Jeong-Ag <kjungu1105@gmail.com>
2025-05-02 20:51:04 +0900

    refactor: 최근 검색어 필터링 추가

--- assignment3/src/app/components/search/SearchArea.jsx
+++ assignment3/src/app/components/search/SearchArea.jsx
@@ -8,37 +8,50 @@ import {
   LogContainer,
 } from "./Search.styles";
 import { LOCAL_STORAGE } from "../../constants/key";
+import { useState } from "react";
 
-const SearchArea = ({
-  inputText,
-  handleInput,
-  handleKeyDown,
-  searchLog,
-  setSearchLog,
-}) => {
+const SearchArea = ({ handleSearch, searchLog, setSearchLog }) => {
+  const [inputText, setInputText] = useState("");
   const handleDeleteClick = (index) => {
     deleteLocalStorage(LOCAL_STORAGE.SEARCH_GITHUB, index);
     setSearchLog(getLocalStorage(LOCAL_STORAGE.SEARCH_GITHUB));
   };
 
+  /**
+   * Enter 키를 누르면 GitHub 사용자 정보를 요청합니다.
+   *
+   * @param {React.KeyboardEvent<HTMLInputElement>} e 키보드 이벤트
+   */
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && inputText.trim()) {
+      handleSearch(inputText.trim());
+      setInputText("");
+    }
+  };
+
   return (
     <SearchContainer>
       <Input
+        type="text"
         text={inputText}
-        handleInputChange={handleInput}
+        handleInputChange={(e) => setInputText(e.target.value)}
         placeholder="Github 프로필을 검색해보세요"
         onKeyDown={handleKeyDown}
       />
       <LogContainer>
         {!!searchLog.length && <p>최근 검색어</p>}
         <LogButtonContainer>
-          {searchLog.map((text, index) => (
-            <SearchLogButton
-              key={text + index}
-              text={text}
-              handleDeleteClick={() => handleDeleteClick(index)}
-            />
-          ))}
+          {searchLog
+            .slice(0, 3)
+            .reverse()
+            .map((text, index) => (
+              <SearchLogButton
+                key={text + index}
+                text={text}
+                onClick={() => handleSearch(text)}
+                handleDeleteClick={() => handleDeleteClick(index)}
+              />
+            ))}
         </LogButtonContainer>
       </LogContainer>
     </SearchContainer>
@@ -46,9 +59,7 @@ const SearchArea = ({
 };
 
 SearchArea.propTypes = {
-  inputText: PropTypes.string.isRequired,
-  handleInput: PropTypes.func.isRequired,
-  handleKeyDown: PropTypes.func.isRequired,
+  handleSearch: PropTypes.func.isRequired,
   searchLog: PropTypes.array.isRequired,
   setSearchLog: PropTypes.func.isRequired,
 };
